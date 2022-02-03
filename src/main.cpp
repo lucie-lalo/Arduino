9accb34b5ec0a135a999842b050031241f955c5a
Lucie Lalo <lucie.lalo@epsi.fr>
2022-02-03 20:45:22 +0100

    Timer Setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,6 @@
 #define LED_MASK (1<<5)
 
 
-void OnLed() {
-  PORTB |= LED_MASK;
-}
-
-void OffLed() {
-  PORTB &= ~LED_MASK;
-}
-
 void toggleLed(){
   PINB |= LED_MASK;
 }
@@ -18,11 +10,21 @@ void ledSetup(){
   DDRB |= LED_MASK;
 }
 
+void timerSetup(){
+  TCCR1A = 0;
+  TCCR1B = (1<< WGM12) | (1<< CS12) | (1<<CS10);
+  OCR1A = 15625;
+  TIMSK1 = (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect){
+  toggleLed();
+}
+
 void setup() {
   ledSetup();
 }
 
 void loop() {
-  toggleLed();
-  delay(1000);
+
 }
